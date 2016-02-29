56d480766d9ce6523f88f05c0cc7a15c00646923
John <jrobdikeman@gmail.com>
2016-02-29 12:34:15 -0600

    read from a file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+// static reserved_operators: Vec<&str> = vec!["pause","output", "input", "finput", "sto", "dupe" ,"+", "-", "/", "*", "%", "?", "goto", "end", "rand", ">", "<", "==", "!="];
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("test.step")
+        .expect("couldn't open file");
+    println!("here now!");
+    let mut s = String::new();
+    file.read_to_string(&mut s)
+        .expect("couldn't read the file");
+
+    // this is a vector of string literals, not Strings
+    let split = s.split_whitespace().collect();
+
+
+}
+
+fn interpret(this: Vec<String>){
+
 }
