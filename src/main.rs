014b7d655b2308c3809076c7cdb6157ca563ce08
John <jrobdikeman@gmail.com>
2016-03-01 22:11:25 -0600

    made the compiler push things onto the stack

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
+#[allow(non_snake_case)]
+
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
-use std::str::Chars;
-#[macro_use]
-extern crate lazy_static;
+// use std::str::Chars;
 
-lazy_static!{
-    static ref reserved_operators: Vec<&'static str> = vec!["pause","output", "input", "finput", "sto", "dupe" ,"+", "-", "/", "*", "%", "?", "goto", "end", "rand", ">", "<", "==", "!="];
-    static ref memory: HashMap<&'static str,Vec<&'static str>> = HashMap::new();
-}
 
 fn main() {
     let mut file = File::open("test.step")
@@ -22,11 +18,17 @@ fn main() {
     // this is a vector of string literals, not Strings
     let split: Vec<&str> = s.split_whitespace().collect();
 
-    interpret(split);
+    interpret(&split);
 }
 
-fn interpret(this: Vec<&str>){
-    println!("{:?}",this);
+fn interpret<'a>(this: &'a Vec<&str>){
+
+    let reserved_operators: Vec<&'static str> = vec!["pause","output", "input", "finput", "sto", "dupe" ,"+", "-", "/", "*", "%", "?", "goto", "end", "rand", ">", "<", "==", "!="];
+
+    let mut memory: HashMap<&'static str,Vec<&str>> = HashMap::new();
+    memory.insert("main",Vec::new());
+
+    // println!("{:?}",this);
     // println!("{}","blah" == String::from("blah"));
     let mut count: u64 = 0;
     loop{
@@ -37,21 +39,30 @@ fn interpret(this: Vec<&str>){
 
 
         match (start_ind,finish_ind){
-            (None,Some(x)) => panic!("didn't start operation #{:?}",count),
-            (Some(x),None) => panic!("never closed operation #{:?}",count),
+            (None,Some(_)) => panic!("didn't start operation #{:?}",count),
+            (Some(_),None) => panic!("never closed operation #{:?}",count),
             // this will happen when the program ends
             (None,None) => break,
-            _ => {
+            (Some(_),Some(_)) => {
                 for ind in start_ind.unwrap()..finish_ind.unwrap() + 1 { // you have to unwrap these because they're Some(thing)
+                    let mut is_step: bool = false;
                     for ch in this[ind].chars(){
-                        if ch == "." {
-                            continue;
+                        // iterate through all the characters in the word and see if theyre a period
+                        if ch == '.' {
+                            // println!("got a number!");
+                            is_step = true;
+                            break;
                         }
+                    }
+                    if !is_step{
+                        //put all the other operator logic in here
+                        let peice: &'a str = this[ind];
+                        memory.get_mut("main").unwrap().push(&*peice);
                     }
                 }
             }
         }
         count += 1;
-
+        println!("{:?}",memory["main"]);
     }
 }
